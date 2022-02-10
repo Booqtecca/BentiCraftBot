b0d258ab0af2f10c50b286d174af37e77505fa59
OPgDev <80653401+OPgDev@users.noreply.github.com>
2022-02-10 16:36:54 -0400

    update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -1,1 +1,16 @@
+const { MessageEmbed } = require("discord.js")
+const config = require("../config.json")
+
+module.exports = {
+ name: "ip",
+ run: async(client, message, args) => {
+ let ip = new MessageEmbed()
+ .setTitle(`Hola se ve que buscas la IP`)
+ .setDescription(`>>>>>>>>>>>>\n\nIp: ${config.ip} \n\nPort: ${config.port}\n\n<<<<<<<<<<<<`)
+ .setColor("RED")
+ .setThumbnail(config.img)
+ 
+ message.channel.send(ip)
+ }
+}
 
