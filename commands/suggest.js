dba3ad122cfac76249662705e68143a2791b9a0f
OPgDev <80653401+OPgDev@users.noreply.github.com>
2022-02-10 17:54:00 -0400

    update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,1 +1,30 @@
+const { MessageEmbed } = require("discord.js")
 
+
+module.exports = {
+    name: "suggest",
+    description: "Make a suggestion and have the community vote",
+    category: "Utility",
+    usage: "i!suggest <suggestion>",
+    run: async (client, message, args) => {
+        let suggestion = args.slice(0).join(" ");
+        let SuggestionChannel = message.guild.channels.cache.find(channel => channel.id === `905277543039528990`);
+      
+      if(!suggestion) {
+        return message.reply("!suggest <sugerencia>")
+      }
+      
+      
+        const embed = new MessageEmbed()
+            .setTitle("New suggest")
+            .setDescription(suggestion)
+            .setColor("RANDOM")
+            .setFooter(`suggestion make by ${message.author.username}`)
+            .setTimestamp()
+        SuggestionChannel.send(embed).then(msg => {
+            msg.react("✅")
+            msg.react("❎")
+        message.channel.send("suggest send");
+        });
+    }
+}
