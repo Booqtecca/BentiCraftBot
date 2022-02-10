0cb8c603d9c6d5b3a0cd15e6ee3549c5655c1a6d
OPgDev <80653401+OPgDev@users.noreply.github.com>
2022-02-10 17:53:32 -0400

    update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,1 +1,30 @@
+const Discord = require("discord.js")
+const config = require("../config.json")
+
+module.exports = {
+name: "say",
+  
+  run: async (client, message, args) => {
+    
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("no tienes permisos para usar este comando")
+  
+    const mensaje = message.content.split(" ").slice(1).join(" ")
+    
+    if(!mensaje) return message.channel.send("que se va a enviar?")
+    
+    await message.delete()
+    
+    let embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setDescription(mensaje)
+    .setThumbnail(`${config.img}?size=dynamic`)
+    
+    message.channel.send(embed)
+    
+    
+    //esperame xd
+    
+    
+  }
+}
 
